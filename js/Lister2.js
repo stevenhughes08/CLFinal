a9d374e58ba5eb2dc12e312d560fa30c5a1767b1
Steven Hughes <StevenHughes08@gmail.com>
2017-07-19 22:23:27 -0400

    completed dynamic characters page.

--- js/Lister2.js
+++ js/Lister2.js
@@ -1,35 +1,40 @@
-//defines clown name array.
-
-
-
-            function(clownNamer){
-              for(i = 0; i < clownName.length; i++){
-                '<li class=' += name[i] + '>';
-              }
-            };
 
 
 //creates a loop that looks for a true value in clownsObj //
 
+
 clownsObj.forEach(function(splat) {
     if (splat.statusActive == true) { //this line create the splat iteration local variable that the results are ported into.//
         console.log(splat.name); //outputs to the log.
-        document.write('<ul class="char">'
-                        + clownNamer();
+        document.write('<div class="row">'
+                        + '<div class="name'
+                        + splat.value
+                        + '">'
                         + splat.name
-                        + '</li>'
-                        + '<li class="biopic">'
+                        + '</div>'
+                        + '<div class="row"'
+                        + '<div class="col-4-lg col-4-md col-1-sm">'
+                        + '<div class="biopic">'
                         + splat.image
-                        + '</li>'
+                        + '</div>'
+                        + '<div class="ccol-4-lg col-4-md col-1-sm">'
+                        + '<ul class="clownList">'
                         + '<li class="tag">'
                         + splat.tagline
                         + '</li>'
-                        + '<li class="disc">'
+                        + '<li class="disc"><p>'
                         + splat.description
-                        + '</li>'
+                        + '</p></li>'
                         + '<li class="actor">'
                         + splat.actor
                         + '</li>'
-                        + '</ul>');
+                        + '</ul>'
+                        + '</div>'
+                        + '</div>'
+                        + '</div>'
+                        + '</div>'
+
+
+                      );
     }
 });
--- js/clowns.js
+++ js/clowns.js
@@ -3,40 +3,43 @@
 var clownsObj = [
 
     {
-        name: 'Simcha D. Clown',
+        name: 'Simcha DClown',
         actor: 'Actor: Steven Hughes',
         statusActive: true,
+        value: '0',
         image: '<img class="biopic" src = "images/CharPics/simcha.jpg">',
         characterType: 'Hobo Clown',
-        tagline: 'Tagline: The clown that offended the gods.',
-        description: 'Simcha is an optimist in a wold of disasters.'
+        tagline: 'The clown that offended the gods.',
+        description: 'Simcha is an optimist in a wold of disasters. Sed sit ipsum conubia posuere ligula, etiam lacus a sollicitudin ad quam, tempus curabitur quam donec sodales neque magnis. Quisque orci eget, venenatis sit vestibulum curabitur orci pede et, tincidunt a sit vestibulum quo ultrices wisi, ut nulla blandit rutrum, feugiat nulla metus. Eros ornare odio mollis mauris donec pulvinar, vitae integer enim, amet ligula dolor in amet eget, mi placerat, posuere in. Soluta pede nec quam morbi curabitur libero. Placerat sociosqu suspendisse dapibus condimentum a eleifend.'
     },
 
     {
         name: 'Bubble-ishus',
         actor: 'Actor: Tatyana Malkin',
         statusActive: true,
+        value: '1',
         image: '<img class="biopic" src = "images/charPics/bubbles.jpg">',
-        characterType: '<div>Whiteface Clown',
-        tagline: '<Your Majesty, Bubble-ishus!',
-        tagline: 'Your Majesty, Bubble-ishus!',
-        description: 'From a dimension where'
+        characterType: 'Whiteface Clown',
+        tagline: 'Vodka!',
+        description: 'From a dimension where. Mi facilisis id duis. Feugiat leo pharetra. Non ut sagittis egestas vitae suscipit, in nunc numquam pellentesque vitae, et erat ut non elit, etiam nisl per arcu elit, neque dui est sed error eu. Vel donec orci, pellentesque erat dapibus justo neque, sollicitudin lacus donec. Magnis tincidunt, volutpat sit vel id vel quam, at vestibulum maiores ipsum eros dui. Lacus dolor convallis praesent sem, metus egestas ut maecenas. Libero sagittis tempor curabitur placerat in tristique, ornare libero arcu, cupiditate sem porttitor et diam. Arcu id varius pulvinar, curabitur diam sed aliquam, dignissim enim et diam curabitur nonummy nibh, quisque luctus scelerisque sed ac urna, ultrices consectetuer malesuada pulvinar turpis consequat velit. Netus cursus nisl, justo sit.'
     },
 
     {
-        name: 'Duke <i>the Germantown Clown</i>',
+        name: 'Duke',
         actor: 'Actor: Athena Prychodko',
         statusActive: true,
+        value: '2',
         image: '<img class="biopic" src = "images/charPics/duke.jpg">',
-        characterType: '<li>Gonzo Clown</li>',
+        characterType: 'Gonzo Clown',
         tagline: 'Borboun!',
-        description: "Duke is the clown manifestation of the late great Hunter S. Thompson Gonzo Journalist and Au      thor. Duke is also the unofficial 'mascot' of Louisville's Gonzofest, a Festival honoring Hunter's legacy.</li>"
+        description: "Duke is the clown manifestation of the late great Hunter S. Thompson Gonzo Journalist and Author. Duke is also the unofficial 'mascot' of Louisville's Gonzofest, a Festival honoring Hunter's legacy. Fusce nisi nunc semper. Sit et sociosqu venenatis condimentum ut lectus, felis etiam et aliquam torquent torquent enim, sit sociis pellentesque eu dis aliquam in, elit etiam. Nascetur eget id nec blandit odio luctus. Eget erat accumsan, integer nonummy scelerisque duis nulla pede, tincidunt ante sit morbi diam, in amet mi nec neque, aenean pellentesque aliquam. Aptent congue enim, ante sapien, mi wisi maecenas quis tincidunt bibendum praesent."
     },
 
     {
         name: 'Dr. Clownzo',
         actor: 'Actor: Steven Hughes',
         statusActive: false,
+        value: '3',
         image: '<img class="biopic" src = "images/charPics/clownzo.jpg">',
         characterType: 'Gonzo clown.',
         tagline: 'As your Attorney, I advsise you...',
@@ -48,26 +51,29 @@ var clownsObj = [
         name: 'Salty',
         actor: 'Actor: Deb Adams',
         statusActive: true,
+        value: '4',
         image: '<img class="biopic" src = "images/charPics/salty.jpg">',
         characterType: '<li>Whiteface Clown</li>',
         tagline: "I don't want the world. I only want your half.",
-        description: "Duke is the clown manifestation of the late great Hunter S. Thompson Gonzo Journalist and Author. Duke is also the unofficial 'mascot' of Louisville's Gonzofest, a Festival honoring Hunter's legacy.</li>"
+        description: "Sed sit ipsum conubia posuere ligula, etiam lacus a sollicitudin ad quam, tempus curabitur quam donec sodales neque magnis. Quisque orci eget, venenatis sit vestibulum curabitur orci pede et, tincidunt a sit vestibulum quo ultrices wisi, ut nulla blandit rutrum, feugiat nulla metus. Eros ornare odio mollis mauris donec pulvinar, vitae integer enim, amet ligula dolor in amet eget, mi placerat, posuere in. Soluta pede nec quam morbi curabitur libero. Placerat sociosqu suspendisse dapibus condimentum a eleifend."
     },
 
     {
         name: 'Lady Luck',
         actor: 'Actor: Katarina Gay',
         statusActive: true,
+        value: '5',
         image: '<img class="biopic" src = "images/charPics/LadyLuck.jpg">',
         characterType: 'Whiteface Clown',
-        tagline: 'May Lady Luck be always by your side.',
-        description: "Lady Luck is fickle, and holds fortunes in her hands, but she cannot hold her booze and loves the vodka."
+        tagline: "You can look but DON'T TOUCH!.",
+        description: "Lady Luck is fickle, and holds fortunes in her hands, but she cannot hold her booze and loves the vodka. Fusce nisi nunc semper. Sit et sociosqu venenatis condimentum ut lectus, felis etiam et aliquam torquent torquent enim, sit sociis pellentesque eu dis aliquam in, elit etiam. Nascetur eget id nec blandit odio luctus. Eget erat accumsan, integer nonummy scelerisque duis nulla pede, tincidunt ante sit morbi diam, in amet mi nec neque, aenean pellentesque aliquam."
     },
 
     {
         name: 'Lady Necrotatas',
         actor: 'Actor: Athena Prychodko',
         statusActive: false,
+        value: '6',
         image: '<img class="biopic" src = "images/charPics/necro.jpg">',
         characterType: 'Rodeo Clown',
         tagline: 'Yee Haw!',
@@ -78,9 +84,10 @@ var clownsObj = [
         name: 'Friendsilly',
         actor: 'Actor: Franceli Sharp',
         statusActive: true,
+        value: '7',
         image: '<img class="biopic" src = "images/charPics/friendsilly.jpg">',
         characterType: 'Violin playing mime',
         tagline: 'Music playing',
-        description: "Friendsilly is an adorable silent mime that can play the fiddle or (Violin) to put Charlie Daniels to shame."
+        description: "Friendsilly is an adorable silent mime that can play the fiddle or (Violin) to put Charlie Daniels to shame. Lorem ipsum dolor sit amet, id cupiditate dolor vestibulum amet, ipsum vitae praesent wisi est, tellus dignissim odio feugiat consectetuer phasellus aliqua, pretium nunc turpis justo sapien wisi, purus sed qui a. Magna rutrum blandit donec, posuere donec, similique lectus. At pede pede et non, vel sagittis blandit velit facilisis dapibus sit. Integer dui wisi molestie vehicula ultricies, nibh diam scelerisque, porttitor est donec. Lacinia lorem. Mi mi maecenas justo. Ut wisi eu risus tincidunt ligula vestibulum, non proin. "
     }
 ];
